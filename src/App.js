117338735b9e01d0b0e36117012a51b361c27efd
cocoder16 <cocoder16@gmail.com>
2021-06-05 18:29:12 +0900

    Add paging
    Add section css

--- src/App.js
+++ src/App.js
@@ -1,12 +1,30 @@
+import { useState } from "react";
+
 import "./App.css";
 import Button from "components/atom/Button";
 
+const dummy = {
+  maxPage: 9, // config - pages
+};
+
 function App() {
+  const [page, setPage] = useState(0);
+
   return (
     <div id="main">
-      <Button value="이전" className="navigation-button" />
+      <Button
+        value="이전"
+        className="navigation-button"
+        disabled={page === 0}
+        onClick={() => setPage(page - 1)}
+      />
       <div id="section"></div>
-      <Button value="다음" className="navigation-button" />
+      <Button
+        value="다음"
+        className="navigation-button"
+        disabled={page === dummy.maxPage}
+        onClick={() => setPage(page + 1)}
+      />
     </div>
   );
 }
--- src/components/atom/Button.js
+++ src/components/atom/Button.js
@@ -1,8 +1,9 @@
-function Button({ value, className, onClick }) {
+function Button({ value, className, disabled, onClick }) {
   return (
     <button
       type="button"
       className={`${className} btn btn-light`}
+      disabled={disabled}
       onClick={event => {
         event.preventDefault();
         onClick && onClick();
