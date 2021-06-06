bf6cf4e707b47376cd1fc6a1f82e272004ab4961
cocoder16 <cocoder16@gmail.com>
2021-06-06 15:37:42 +0900

    Add example 3

--- src/components/section/examples/index.js
+++ src/components/section/examples/index.js
@@ -1,5 +1,6 @@
 import Example1 from "components/section/examples/example1";
 import Example2 from "components/section/examples/example2";
+import Example3 from "components/section/examples/example3";
 
 function Example({ page }) {
   switch (page) {
@@ -7,6 +8,8 @@ function Example({ page }) {
       return <Example1 />;
     case 1:
       return <Example2 />;
+    case 2:
+      return <Example3 />;
     default:
       return <div />;
   }
