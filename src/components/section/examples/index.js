6f41065956eb33b99c9e05da68c82a03c0f7ac83
cocoder16 <cocoder16@gmail.com>
2021-06-06 15:57:11 +0900

    Add example 4

--- src/components/section/examples/index.js
+++ src/components/section/examples/index.js
@@ -1,6 +1,7 @@
 import Example1 from "components/section/examples/example1";
 import Example2 from "components/section/examples/example2";
 import Example3 from "components/section/examples/example3";
+import Example4 from "components/section/examples/example4";
 
 function Example({ page }) {
   switch (page) {
@@ -10,6 +11,8 @@ function Example({ page }) {
       return <Example2 />;
     case 2:
       return <Example3 />;
+    case 3:
+      return <Example4 />;
     default:
       return <div />;
   }
--- src/components/section/header/index.js
+++ src/components/section/header/index.js
@@ -1,12 +1,14 @@
 import pages from "config/pages";
 
 function Header({ page }) {
+  const id = pages[page].id;
   const title = pages[page].title;
   const explanation = pages[page].explanation;
 
   return (
     <div id="header">
-      <h1>{title}</h1>
+      <h1>{`Example ${id + 1}`}</h1>
+      <h2>{title}</h2>
       <ul>
         {explanation.map((item, index) => {
           return <li key={index}>{item}</li>;
